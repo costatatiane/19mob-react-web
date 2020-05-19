9264439fa922798270a89187f5bb8c7ab9885217
Tatiane <tati.d.costa@gmail.com>
2020-05-18 22:40:03 -0300

    Implement unit tests

---
+++ src/components/Button/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Button from '../index';
+
+describe('Button', () => {
+    // it('should be render button with label Test', () => {
+    //     const element = renderer.create(<Button label="Test" />); 
+    //     expect(element.toJSON()).toMatchSnapshot();
+    // });
+
+    // it('should be className equal btn', () => {
+    //     const element = renderer.create(<Button label="Test" />); 
+    //     expect(element).toHaveAttribute('class', 'btn');
+    // });
+});
