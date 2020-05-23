c0548ed2913658ec67e53e1a3d99760c3a5b349a
Tatiane <tati.d.costa@gmail.com>
2020-05-23 09:00:19 -0300

    Test button

--- src/components/Button/__tests__/index.test.js
+++ src/components/Button/__tests__/index.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 
 import Button from '../index';
 
-describe('Button', () => {
-    // it('should be render button with label Test', () => {
-    //     const element = renderer.create(<Button label="Test" />); 
-    //     expect(element.toJSON()).toMatchSnapshot();
-    // });
+let element = null;
 
-    // it('should be className equal btn', () => {
-    //     const element = renderer.create(<Button label="Test" />); 
-    //     expect(element).toHaveAttribute('class', 'btn');
-    // });
+describe('Button', () => {
+    beforeEach(() => {
+        element = render(<Button label="Test" />);
+    });
+
+    it('should be render button with label Test', () => {
+        const { container } = element;
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should be class equal btn', () => {
+        const { container } = element;
+        expect(container.querySelector('button').className).toBe('btn')
+    });
+
+    it('should be label equal Text', () => {
+        const { container } = element;
+        expect(container.querySelector('button').innerHTML).toBe('Test')
+    });
 });
